77e3d5b8ff376e59fb4642bba4eab0200203e805
suyogadhikari <adhikarisuyog49@gmail.com>
2022-11-18 21:30:04 +0545

    basic nft contract with test

--- token_contract.py
+++ token_contract.py
@@ -1,34 +1,37 @@
 import smartpy as sp
+
 FA2 = sp.io.import_script_from_url("https://smartpy.io/templates/fa2_lib.py")
 
-class NftWithAdmin(FA2.Admin, FA2.Fa2Nft, FA2.OnchainviewBalanceOf, 
-FA2.WithdrawMutez):
+
+class NftWithAdmin(FA2.Admin, FA2.Fa2Nft, FA2.OnchainviewBalanceOf,
+                   FA2.WithdrawMutez):
     def __init__(self, admin, **kwargs):
         FA2.Fa2Nft.__init__(self, **kwargs)
         FA2.Admin.__init__(self, admin)
         self.update_initial_storage(
-            token_price = sp.mutez(0),
-            whitelist = sp.set([], t = sp.TAddress)
+            token_price=sp.mutez(0),
+            whitelist=sp.set([], t=sp.TAddress)
         )
-    
+
     @sp.entry_point
     def add_whitelist(self, params):
         """Add whitelist address through admin."""
         sp.verify(self.is_administrator(sp.sender), "Only Admin can whitelist the address.")
-        sp.for x in params:
+        sp.
+        for x in params:
             sp.verify(~self.data.whitelist.contains(x),
-            message="Already Whitelisted")
+                      message="Already Whitelisted")
             self.data.whitelist.add(x)
             # event log emitted
             sp.emit(x)
-    
+
     @sp.entry_point
     def set_token_price(self, price_in_mutez):
         """Sets token price for NFT."""
         sp.verify(self.is_administrator(sp.sender), "Only Admin can set the nft price.")
         self.data.token_price = price_in_mutez
 
-    @sp.entry_point(check_no_incoming_transfer = False)
+    @sp.entry_point(check_no_incoming_transfer=False)
     def mint(self, batch):
         """ A very simple implementation of the mint entry point"""
         sp.verify(sp.amount == self.data.token_price, "Invalid Price")
@@ -44,12 +47,12 @@ FA2.WithdrawMutez):
         )
         with sp.for_("action", batch) as action:
             token_id = sp.compute(self.data.last_token_id)
-            metadata = sp.record(token_id=token_id, 
-token_info=action.metadata)
+            metadata = sp.record(token_id=token_id,
+                                 token_info=action.metadata)
             self.data.token_metadata[token_id] = metadata
             self.data.ledger[token_id] = action.to_
             self.data.last_token_id += 1
-    
+
     @sp.entry_point
     def burn(self, batch):
         """Users can burn tokens if they have the transfer policy 
@@ -88,19 +91,20 @@ token_info=action.metadata)
         sp.verify(self.is_defined(params.token_id), "FA2_TOKEN_UNDEFINED")
         sp.result(sp.set_type_expr(self.supply_(params.token_id), sp.TNat))
 
+
 @sp.add_test(name="NFT with mint and burn")
 def test():
     sc = sp.test_scenario()
     admin = sp.address("tz1euHP1ntD4r3rv8BsE5pXpTRBnUFu69wYP")
     alice = sp.address("tz1g3pJZPifxhN49ukCZjdEQtyWgX2ERdfqP")
     token_price = sp.mutez(1000000)
-    
+
     # deploy a contract
     c1 = NftWithAdmin(
-        admin = admin,
-        metadata = sp.utils.metadata_of_url("https://example2.com"),
-        policy = FA2.PauseTransfer()
-        )
+        admin=admin,
+        metadata=sp.utils.metadata_of_url("https://example2.com"),
+        policy=FA2.PauseTransfer()
+    )
     sc += c1
 
     sc.h2("Set token price")
@@ -109,78 +113,77 @@ def test():
 
     sc.h2("whitelist the admin address")
     c1.add_whitelist([admin]
-    ).run(sender = admin)
+                     ).run(sender=admin)
     sc.verify_equal(c1.data.whitelist, sp.set([admin]))
 
     sc.h2("whitelist the alice address")
     c1.add_whitelist([alice]
-    ).run(sender = admin)
+                     ).run(sender=admin)
     sc.verify_equal(c1.data.whitelist, sp.set([admin, alice]))
 
     sc.h2("Whitelist called by alice instead of admin")
     c1.add_whitelist([alice]
-    ).run(sender = alice, valid = False, exception ="Only Admin can whitelist the address." )
-    
+                     ).run(sender=alice, valid=False, exception="Only Admin can whitelist the address.")
+
     sc.h2("Mint a token without sending tez")
     c1.mint([
-    sp.record(
-    to_ = admin, metadata = sp.map({
-    "test":sp.utils.bytes_of_string("ipfs//test_uri")}))
+        sp.record(
+            to_=admin, metadata=sp.map({
+                "test": sp.utils.bytes_of_string("ipfs//test_uri")}))
     ]
-    ).run(sender = admin, valid = False, exception = "Invalid Price")
+    ).run(sender=admin, valid=False, exception="Invalid Price")
 
     # last token ID before minting
     sc.verify_equal(c1.data.last_token_id, 0)
-    
+
     sc.h2("Mint a token to admin address")
     c1.mint([
-    sp.record(
-    to_ = admin, metadata = sp.map({
-    "test":sp.utils.bytes_of_string("ipfs//test_uri")}))
+        sp.record(
+            to_=admin, metadata=sp.map({
+                "test": sp.utils.bytes_of_string("ipfs//test_uri")}))
     ]
-    ).run(sender = admin, amount = token_price)
+    ).run(sender=admin, amount=token_price)
     sc.verify_equal(c1.data.last_token_id, 1)
     sc.verify_equal(c1.data.ledger[0], admin)
     sc.verify_equal(c1.data.ledger.contains(0), True)
 
     sc.h2("Mint a token to alice address")
     c1.mint([
-    sp.record(
-    to_ = alice, metadata = sp.map({
-    "test":sp.utils.bytes_of_string("ipfs//test_uri")}))
+        sp.record(
+            to_=alice, metadata=sp.map({
+                "test": sp.utils.bytes_of_string("ipfs//test_uri")}))
     ]
-    ).run(sender = alice, amount = token_price)
+    ).run(sender=alice, amount=token_price)
     sc.verify_equal(c1.data.last_token_id, 2)
     sc.verify_equal(c1.data.ledger[1], alice)
 
     sc.h2("Transfers a token")
     c1.transfer([
-    sp.record(
-    from_ = admin,
-    txs = [ sp.record( amount = 1, token_id = 0, to_ = alice)])
+        sp.record(
+            from_=admin,
+            txs=[sp.record(amount=1, token_id=0, to_=alice)])
     ]
-    ).run(sender = admin)
+    ).run(sender=admin)
     sc.verify_equal(c1.data.last_token_id, 2)
     sc.verify_equal(c1.data.ledger[0], alice)
     sc.verify_equal(c1.data.ledger[1], alice)
 
     sc.h2("Burn a token")
     c1.burn([
-    sp.record(
-    amount = 1, token_id = 0, from_ = alice)
+        sp.record(
+            amount=1, token_id=0, from_=alice)
     ]
-    ).run(sender = alice)
+    ).run(sender=alice)
     sc.verify_equal(c1.data.ledger.contains(0), False)
 
     sc.h2("Pause the transfer")
-    c1.set_pause(True).run(sender = admin)
+    c1.set_pause(True).run(sender=admin)
 
     sc.h2("Transfers a token during pause")
     c1.transfer([
-    sp.record(
-    from_ = alice,
-    txs = [ sp.record( amount = 1, token_id = 1, to_ = admin)])
+        sp.record(
+            from_=alice,
+            txs=[sp.record(amount=1, token_id=1, to_=admin)])
     ]
-    ).run(sender = alice, valid = False, exception = ('FA2_TX_DENIED', 
-'FA2_PAUSED'))
-
+    ).run(sender=alice, valid=False, exception=('FA2_TX_DENIED',
+                                                'FA2_PAUSED'))
